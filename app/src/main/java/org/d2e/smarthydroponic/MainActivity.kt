9805140996638f4279d8d09c61cb4ea2ebe313cc
alfath abibi <alfath.abibi@gmail.com>
2021-03-18 16:34:28 +0700

    Adding Nutrition Activity

--- app/src/main/java/org/d2e/smarthydroponic/MainActivity.kt
+++ app/src/main/java/org/d2e/smarthydroponic/MainActivity.kt
@@ -1,11 +1,19 @@
 package org.d2e.smarthydroponic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cvSetTemperature.setOnClickListener {
+            Intent(this, TemperatureActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/org/d2e/smarthydroponic/TemperatureActivity.kt
+++ app/src/main/java/org/d2e/smarthydroponic/TemperatureActivity.kt
@@ -1,11 +1,18 @@
 package org.d2e.smarthydroponic
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_temperature.*
 
 class TemperatureActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_temperature)
+
+        ivBackTemperature.setOnClickListener {
+            finish()
+        }
     }
 }

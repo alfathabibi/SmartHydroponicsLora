b2725763d5948b7dbd6127227067f79f684d6faf
alfath abibi <alfath.abibi@gmail.com>
2021-03-30 19:14:50 +0700

    add login activity

--- app/src/main/java/org/d2e/smarthydroponic/auth/LoginActivity.kt
+++ app/src/main/java/org/d2e/smarthydroponic/auth/LoginActivity.kt
@@ -3,17 +3,17 @@ package org.d2e.smarthydroponic.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_login.*
 import org.d2e.smarthydroponic.R
 import org.d2e.smarthydroponic.process.PhActivity
 
-class RegisterActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
 
-        tvLoginHere.setOnClickListener {
-            Intent(this, LoginActivity::class.java).also {
+        tvSignup.setOnClickListener {
+            Intent(this, RegisterActivity::class.java).also {
                 startActivity(it)
                 finish()
             }
